fd5204e25bb5b54995a028d0a307f8862284c4c1
miiaevia <miiaevia@gmail.com>
2018-03-03 11:40:34 -0700

    MVP clean format and comment

--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -33,8 +33,10 @@ public class MakeChangeApp {
 	static Scanner kb = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		
+		// get price and amount tendered 
 		priceAmount();
+		// evaluate to see if change is needed and how much, or more money is needed
 		calcChange();
 
 		kb.close();
@@ -49,54 +51,54 @@ public class MakeChangeApp {
 	}
 
 	public static void calcChange() {
+		// if exact amount provided, nothing else  needed
 		if (price == custAmount) {
 			System.out.println("Thank you, have a good day.");
 		}
 		else if (price > custAmount) {
+			// if not enough money provided, give error
 			System.err.println("The amount provided is less than the item price. ");
-			System.out.println("You still owe "+ -(custAmount - price));
+			System.out.println("You still owe " + -(custAmount - price));
 			// System.err.println("Please provide a new amount.");
 		}
 		else {
+			// calc how much change is needed
 			change = Math.round(((custAmount - price) * 100));
 			System.out.println("Thank you. Your change is: ");
+			// use method to calc exact bills/coins
 			calcRemainder();
 		}
 	}
+
 	public static void calcRemainder() {
+
+		// stores name values of change
+		String[] bill = { "20", "10", "5", "1", "Quarter", "Dime", "Nickel", "Penny" };
+		// stores values of change
+		int[] values = { 2000, 1000, 500, 100, 25, 10, 5, 1 };
 		
-		
-		String[] bill = {"20", "10", "5", "1", "Quarter", "Dime", "Nickel", "Penny"};
-//		String[] coins = {"Quarters", "Dimes", "Nickels", "Pennies"};
-//		String[] coin = {"Quarter", "Dime", "Nickel", "Penny"};
-		int[] values = {2000, 1000, 500, 100, 25, 10, 5, 1};
-		
+		// loop through change calculations
 		for (int i = 0; i < values.length; i++) {
-			int numberChange = Math.round((int)(change/values[i]));
+			// figure out amount of each bill/coin type
+			int numberChange = Math.round((int) (change / values[i]));
+			// if plural bills are needed, add an "s" (except pennies)
 			if ((values[i] >= 1) && (numberChange != 1)) {
 				if (values[i] != 1) {
-					System.out.println( numberChange + " " + bill[i] + "s");					
+					System.out.println(numberChange + " " + bill[i] + "s");
 				}
+				// (if plural pennies are needed, print "Pennies"
 				else {
 					System.out.println(numberChange + " Pennies");
 				}
+				// recalc change so it only loops through remainder
 				change = change - (numberChange * values[i]);
 			}
-			else if ((values[i] >= 1 ) && (numberChange == 1)) {
-				System.out.println( numberChange + " " + bill[i]);
+			// if bills are singular, do not print "s"
+			else if ((values[i] >= 1) && (numberChange == 1)) {
+				System.out.println(numberChange + " " + bill[i]);
+				// recalc change so it only loops through remainder
 				change = change - (numberChange * values[i]);
-			}
-//			else if ((values[i] < 1) && (numberChange > 1)) {
-//				System.out.println( numberChange + " " + coin[i]);
-//			}
-//			else if ((values[i] < 1) && (numberChange <= 1)) {
-//				System.out.println( numberChange + " " + coin[i]);
 			}
 		}
 	}
-
-
-	// public static void remainder() {
-	//
-	// }
-
+}
