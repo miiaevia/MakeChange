d652c3e9b2eaf3d9bba00e346a996e7efbddbcd7
miiaevia <miiaevia@gmail.com>
2018-03-02 17:25:55 -0700

    first commit of MakeChangeApp.java

---
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.projects.makechange;
+
+public class MakeChangeApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0; 
+		System.out.println(i);
+	}
+
+}
